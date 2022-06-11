3ef0566a2b099a2e37d1fea310928eb36729bc18
RobYang203 <101m06014@stud.sju.edu.tw>
2022-06-12 06:06:10 +0800

    feat: init

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const POST = 3000;
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World! <br/> Hello world')
+})
+
+
+app.listen(POST);
