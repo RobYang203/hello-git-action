801eab66f1433a28d45355c620ea05c8b23364ef
RobYang203 <101m06014@stud.sju.edu.tw>
2022-06-26 21:44:23 +0800

    update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const POST = 3000;
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World! <br/> Hello world')
+    res.send('Hello World~~~! <br/> Hello world')
 })
 
 console.log('test...' , process.env);
